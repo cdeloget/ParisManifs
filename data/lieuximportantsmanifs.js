a49ff1bf397a3ad0c2bd5443caa34e349cace806
CD <dcyd@protonmail.me>
2023-06-07 00:23:04 +0200

    fond de carte Paris

---
+++ data/lieuximportantsmanifs.js
@@ -0,0 +1,16 @@
+const lieuxmanifs = {
+"type": "FeatureCollection",
+"name": "lieuximportantsmanifs",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "toponyme": "Châtelet", "type": "lieu central" }, "geometry": { "type": "Point", "coordinates": [ 2.350554963010042, 48.859069214459907 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Opéra", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.33232406507598, 48.870031276959608 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Concorde", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.320915051530148, 48.86539710032347 ] } },
+{ "type": "Feature", "properties": { "toponyme": "République", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.363822351958182, 48.867561284336297 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Bastille", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.369138717033147, 48.853258850860286 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Nation", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.396143969714825, 48.848271818135096 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Invalides", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.3126699809161, 48.857081458491614 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Place d'Italie", "type": null }, "geometry": { "type": "Point", "coordinates": [ 2.355624328822673, 48.831346487730499 ] } },
+{ "type": "Feature", "properties": { "toponyme": "Denfert", "type": "place" }, "geometry": { "type": "Point", "coordinates": [ 2.332135875161824, 48.834439859444487 ] } }
+]
+}
---
+++ js/carto.js
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////
+//---------------- Création de la carte avec d3js ---------------
+///////////////////////////////////////////////////////////////////
+
+//dimensions de la carte
+const mapwidth = window.innerWidth * 0.9, mapheight = window.innerHeight * 0.95;
+
+//définition d'un objet géographique (geoPath) et d'une projection
+
+const map = d3.geoPath();
+const proj = d3.geoMercator()
+    .center([2.28, 48.88])
+	.scale(400000);
+map.projection(proj);
+
+//ajout d'un groupe svg à la div qui doit contenir la carte
+
+const svg = d3.select('#carto').append("svg")
+  .attr("id", "svgcarte")
+  .attr("width", mapwidth)
+  .attr("height", mapheight);
+
+
+//Ajout des couches de données
+
+    //ajout d'un groupe svg contenant le fond de carte Paris
+    const paris = svg.append('g');
+
+    paris.selectAll("path")
+        .data(basemapparis.features)
+        .enter()
+        .append("path")
+        .attr("d", map)
+        // Sémiologie (par défaut) des objets
+        .style("fill", "#363939")
+        .style("stroke-width", 2);
+
+    //Idem pourr ajouter certains toponymes (figuré ponctuel)
+    const localites = svg.append('g');
+
+    localites.selectAll("path")
+        .data(lieuxmanifs.features)
+        .enter()
+        .append("path")
+        .attr("d", map)
+        .style("fill", "gray");
+
+
+    //Placement du texte des toponymes
+    const labels = svg.append('g');
+
+    labels.selectAll(".place-label")
+        .data(lieuxmanifs.features)
+        .enter()
+        .append("text")
+        .attr("class", "labelmap")
+        .attr("x", (d)=>{return map.centroid(d)[0] + 5} )
+        .attr("y", (d)=>{return map.centroid(d)[1] - 15} )
+        .text((d)=>{return d.properties.toponyme;} );
+
+    console.log(lieuxmanifs.features[1].geometry.coordinates[0])
+
+
